f08233336476364a1de9165efa4debad0a0e7285
Maheshbabu <nadigotimahesh@gmail.com>
2022-09-06 16:12:18 +0530

    card changes

--- src/main/java/com/example/ebanking/controller/SecurityController.java
+++ src/main/java/com/example/ebanking/controller/SecurityController.java
@@ -25,7 +25,7 @@ import com.example.ebanking.service.UserService;
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:4200")
-//security controller one
+//security controller two
 public class SecurityController {
 	
 	@Autowired
