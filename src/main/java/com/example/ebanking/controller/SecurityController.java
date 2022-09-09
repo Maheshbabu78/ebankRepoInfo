4c5a310ec39565f40520721876f6ad8cb026f449
Maheshbabu <nadigotimahesh@gmail.com>
2022-09-09 13:00:35 +0530

    admin changes security

--- src/main/java/com/example/ebanking/controller/SecurityController.java
+++ src/main/java/com/example/ebanking/controller/SecurityController.java
@@ -25,7 +25,7 @@ import com.example.ebanking.service.UserService;
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:4200")
-//security controller eight
+//security controller nine
 public class SecurityController {
 	
 	@Autowired
