e61156cee4d01544db23b643cc43afbb5e21a660
Maheshbabu <nadigotimahesh@gmail.com>
2022-09-09 18:32:21 +0530

    admin changes security c

--- src/main/java/com/example/ebanking/controller/AdminController.java
+++ src/main/java/com/example/ebanking/controller/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/admin")
 @CrossOrigin(origins = "http://localhost:4200")
-//controller three
+//controller four
 public class AdminController {
 
     @Autowired
